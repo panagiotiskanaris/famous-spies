435f93d6e2c5bd2b8c56d596918ae0008615d6cb
Panagiotis K <pntkanaris@gmail.com>
2025-02-25 21:48:56 +0200

    Spy Creation Change

--- app/Actions/Spy/CreateSpyAction.php
+++ app/Actions/Spy/CreateSpyAction.php
@@ -10,6 +10,13 @@ class CreateSpyAction
 {
     public function handle(array $spyData): Spy
     {
-        return Spy::query()->create($spyData);
+        return Spy::query()->create([
+            'name' => $spyData['name'],
+            'surname' => $spyData['surname'],
+            'date_of_birth' => $spyData['date_of_birth'],
+            'country_of_operation' => $spyData['country_of_operation'] ?? null,
+            'date_of_death' => $spyData['date_of_death'] ?? null,
+            'agency_id' => $spyData['agency_id'] ?? null,
+        ]);
     }
 }
