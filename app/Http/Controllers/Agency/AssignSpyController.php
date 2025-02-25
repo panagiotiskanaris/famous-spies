d50412637fa0177cd7826911f3d4ef0853966b27
Panagiotis K <pntkanaris@gmail.com>
2025-02-25 21:34:31 +0200

    Spy cannot be assigned to the same Agency

--- app/Http/Controllers/Agency/AssignSpyController.php
+++ app/Http/Controllers/Agency/AssignSpyController.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Agency;
 
+use App\Exceptions\AssignSpyException;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Agency\AgencyResource;
 use App\Models\Agency;
 use App\Models\Spy;
 use App\Services\AgencyService;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class AssignSpyController extends Controller
 {
+    /**
+     * @throws Throwable
+     */
     public function __invoke(Agency $agency, Spy $spy, AgencyService $service): AgencyResource
     {
-        $agency = $service->assignSpyToAgency($spy, $agency);
+        try {
+            $agency = $service->assignSpyToAgency($spy, $agency);
+        } catch (AssignSpyException $exception) {
+            abort(Response::HTTP_BAD_REQUEST, $exception->getMessage());
+        }
 
         return AgencyResource::make($agency);
     }
--- app/Services/AgencyService.php
+++ app/Services/AgencyService.php
@@ -2,13 +2,23 @@
 
 namespace App\Services;
 
+use App\Exceptions\AssignSpyException;
 use App\Models\Agency;
 use App\Models\Spy;
+use Throwable;
 
 class AgencyService
 {
+    /**
+     * @throws Throwable
+     */
     public function assignSpyToAgency(Spy $spy, Agency $agency): Agency
     {
+        throw_if(
+            $spy->agency_id === $agency->id,
+            new AssignSpyException('The selected Spy is already assigned to this agency.'),
+        );
+
         $agency->spies()->save($spy);
 
         return $agency->load('spies');
